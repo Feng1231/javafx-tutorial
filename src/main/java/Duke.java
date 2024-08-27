565087d5f2ccf135d5ef121584ac39f2ee4b31fc
Feng1231 <chenfeng021231@gmail.com>
2024-08-27 21:36:41 +0800

    Add DialogBox.java, move code in Duke.java to Main.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,12 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
+public class Duke {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
 
-public class Duke extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!!!!!!!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
